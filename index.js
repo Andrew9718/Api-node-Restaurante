5ddbfc5bfba2acb2da5d9ae02f28578c295819df
Andrew9718 <josea1803@hotmail.com>
2024-02-28 19:24:54 +0100

    nueva version

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 const Restaurantes = require("./routes/restaurantes");
 app.use(express.json());
+
 app.use(
     express.urlencoded({
         extended: true,
     })
 );
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/", (req, res) => {
     res.json({ message: "ok" });
 });
